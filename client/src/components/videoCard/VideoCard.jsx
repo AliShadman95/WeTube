a089a6bf062cee978ff7107923a62d6802600159
flaminia <flaminia.tumino@sourcesense.com>
2020-05-15 15:50:41 +0200

    added VideoCard component

--- client/src/components/videoCard/VideoCard.jsx
+++ client/src/components/videoCard/VideoCard.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
 import image from "./img/ezioBosso.jpeg";
 
 
 
+function VideoCard() {
+    return (
+        <Card>
+            <Card.Img variant="top" src={image} />
+            <Card.Body>
+                <Card.Title>Ezio Bosso: Rain, In Your Black Eyes</Card.Title>
+                <Card.Text>
+                    Ezio Bosso
+      </Card.Text>
+                <Card.Text>
+                    Data di Pubblicazione
+      </Card.Text>
+            </Card.Body>
+        </Card>);
+}
 
-<Card>
-    <Card.Img variant="top" src={image} />
-    <Card.Body>
-        <Card.Title>Ezio Bosso: Rain, In Your Black Eyes</Card.Title>
-        <Card.Text>
-            Ezio Bosso
-      </Card.Text>
-        <Card.Text>
-            Data di Pubblicazione
-      </Card.Text>
-    </Card.Body>
-</Card>
+export default VideoCard;
