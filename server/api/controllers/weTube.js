5b942cc582fbc89fbfe62710ed775df6586b9ce7
Ali Shadman <ali.shadman@sourcesense.com>
2020-05-15 15:43:24 +0200

    added some routes

--- server/api/controllers/weTube.js
+++ server/api/controllers/weTube.js
@@ -1,17 +1,46 @@
-"use strict";
-import axios from "axios";
-
+const axios = require("axios");
 const apiKey = process.env.API_KEY;
 
 exports.list_most_popular_videos = async (req, res) => {
-  // chiamata al api di youtube
+  let id = req.params.id;
   try {
     let data = await axios.get(
-      `https://www.googleapis.com/youtube/v3/videos?part=player%2Csnippet%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IT&key=${apiKey}`
+      `https://www.googleapis.com/youtube/v3/videos?part=player%2Csnippet%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IT&${
+        typeof id !== "undefined" ? `videoCategoryId=${id}&` : ""
+      }key=${apiKey}`
     );
 
-    console.log(data);
-    res.send(data);
+    console.log(data.data);
+    res.send(data.data);
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+exports.list_videos_by_search = async (req, res) => {
+  let searchValue = req.params.title;
+  try {
+    let data = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&q=${searchValue}&regionCode=IT&key=[YOUR_API_KEY]`
+    );
+
+    console.log(data.data);
+    res.send(data.data);
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+exports.list_comments_by_video = async (req, res) => {
+  // chiamata al api di youtube
+  const id = "0";
+  try {
+    let data = await axios.get(
+      `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&order=relevance&videoId=EZXvdPZx-PY&key=${apiKey}`
+    );
+
+    console.log(data.data);
+    res.send(data.data);
   } catch (error) {
     res.send(error);
   }
--- server/api/ruotes/WeTubeRoutes.js
+++ server/api/ruotes/WeTubeRoutes.js
@@ -3,4 +3,7 @@ module.exports = (app) => {
   var weTube = require("../controllers/weTube");
 
   app.route("/videos/most-popular").get(weTube.list_most_popular_videos);
+  app.route("/videos/most-popular/:id").get(weTube.list_most_popular_videos);
+  app.route("/videos/search/:title").get(weTube.list_most_popular_videos);
+  app.route("/comments/:videoId").get(weTube.list_comments_by_video);
 };
