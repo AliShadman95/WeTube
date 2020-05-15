1cef40da712cdad843efdc17407c0a6b7a261e88
Marco Cuomo <marco.cuomo@sourcesense.com>
2020-05-15 19:56:39 +0200

    update format video

--- client/src/components/videoPage/Video.jsx
+++ client/src/components/videoPage/Video.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 function Video({ src }) {
   return (
-    <div>
+    <div
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%" /* 16:9 */,
+        paddingTop: 25,
+        height: 0,
+      }}
+    >
       <iframe
-        width="100%"
-        height="500px"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
         src={src}
         frameborder="1"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
