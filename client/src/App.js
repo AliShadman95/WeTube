8eeba8d7372310859812f1886071577796d6788f
Ali Shadman <ali.shadman@sourcesense.com>
2020-05-15 16:32:19 +0200

    Updated some files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Main from "./components/Main";
 import VideoPageContent from "./container/videoPage/VideoPageContent.jsx";
-import "./App.css";
+import "./styles/main.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/index";
