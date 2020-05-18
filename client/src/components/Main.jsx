890a188e1d57aeb4f1fc296e2e3212ce6c8a64be
flaminia <flaminia.tumino@sourcesense.com>
2020-05-18 16:30:53 +0200

    added navbar

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import VideoContainer from "./videoContainer/VideoContainer.jsx"
+import VideoContainer from "./videoContainer/VideoContainer.jsx";
+import Menu from "./menu/Menu.jsx";
 
 function Main() {
-  return <div>
-    <h1>Wetube!</h1>
-    <VideoContainer />
-    <VideoContainer />
-    <VideoContainer />
-  </div>;
+  return (
+    <div>
+      <Menu />
+      <VideoContainer />
+      <VideoContainer />
+      <VideoContainer />
+    </div>
+  );
 }
 
 export default Main;
