81da72beaa26ca4a4edc2ea03596027f03e3f0e5
Ali Shadman <ali.shadman@sourcesense.com>
2020-05-19 16:26:53 +0200

    small fix

--- client/src/container/verticalListPages/VerticalListPages.jsx
+++ client/src/container/verticalListPages/VerticalListPages.jsx
@@ -6,6 +6,7 @@ import { connect } from "react-redux";
 import { getVideos } from "../actions/videoActions";
 
 function VerticalListPages({ match, getVideos, videoTrendsList }) {
+  console.log(videoTrendsList);
   useEffect(() => {
     if (match.params.searchValue) {
       // Need to search with value
@@ -17,20 +18,21 @@ function VerticalListPages({ match, getVideos, videoTrendsList }) {
   return (
     <Container fluid className="mt-3">
       <Row>
-        {videoTrendsList.items.map((video) => (
-          <Col md="12" className="mb-2" key={video.id}>
-            <RelatedVideo
-              id={video.id}
-              isSearchPage
-              src={video.snippet.thumbnails.medium.url}
-              titolo={video.snippet.title}
-              nomecanale={video.snippet.channelTitle}
-              view={video.statistics.viewCount}
-              data={video.snippet.publishedAt}
-              description={video.snippet.description}
-            />
-          </Col>
-        ))}
+        {typeof videoTrendsList !== "undefined" &&
+          videoTrendsList.items.map((video) => (
+            <Col md="12" className="mb-2" key={video.id}>
+              <RelatedVideo
+                id={video.id}
+                isSearchPage
+                src={video.snippet.thumbnails.medium.url}
+                titolo={video.snippet.title}
+                nomecanale={video.snippet.channelTitle}
+                view={video.statistics.viewCount}
+                data={video.snippet.publishedAt}
+                description={video.snippet.description}
+              />
+            </Col>
+          ))}
       </Row>
     </Container>
   );
