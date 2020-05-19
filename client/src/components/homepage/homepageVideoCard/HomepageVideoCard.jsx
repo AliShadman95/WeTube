8f9143c43fc0fc4113d01682f026238bca06751c
Ali Shadman <ali.shadman@sourcesense.com>
2020-05-19 10:14:02 +0200

    Some changes

--- client/src/components/homepage/homepageVideoCard/HomepageVideoCard.jsx
+++ client/src/components/homepage/homepageVideoCard/HomepageVideoCard.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import image from "./img/ezioBosso.jpeg";
 
-function VideoCard() {
+function HomepageVideoCard({ src, title, channelTitle, date }) {
   return (
-    <Card>
-      <Card.Img variant="top" src={image} />
+    <Card className="ml-3">
+      <Card.Img variant="top" src={src} />
       <Card.Body>
-        <Card.Title>Ezio Bosso: Rain, In Your Black Eyes</Card.Title>
-        <Card.Text>Ezio Bosso</Card.Text>
-        <Card.Text>Data di Pubblicazione</Card.Text>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{channelTitle}</Card.Text>
+        <Card.Text>{date}</Card.Text>
       </Card.Body>
     </Card>
   );
 }
 
-export default VideoCard;
+export default HomepageVideoCard;
--- client/src/components/homepage/homepageVideoContainer/HomepageVideoContainer.jsx
+++ client/src/components/homepage/homepageVideoContainer/HomepageVideoContainer.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Container from "react-bootstrap/Container";
-import CardGroup from "react-bootstrap/CardGroup";
+import { Container, CardGroup } from "react-bootstrap";
 import VideoCard from "../homepageVideoCard/HomepageVideoCard.jsx";
+import image from "../homepageVideoCard/img/ezioBosso.jpeg";
 
 function HomepageVideoContainer({ title }) {
   return (
     <Container className="pt-4 pb-4">
       <h3 className="white-text">{title}</h3>
       <CardGroup>
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
+        {["", "", "", ""].map((video) => {
+          return (
+            <VideoCard
+              src={image}
+              title="Ezio Bosso: Rain, In Your Black Eyes"
+              channelTitle="Ezio Bosso"
+              date="03/11/2020"
+            />
+          );
+        })}
       </CardGroup>
     </Container>
   );
