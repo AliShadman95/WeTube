379b1a9b8231a2127bfa9ebf287578aaf9c6df70
Ali Shadman <ali.shadman@sourcesense.com>
2020-05-20 16:24:14 +0200

    added build

---
+++ client/build/precache-manifest.aaa1869fcb379aad4a7c439113b9c164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0edf1347312461541f63009a22c2f10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4baf5c08a63efaafe193",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e665e3d0f65ec67d9273",
+    "url": "/static/css/main.55120326.chunk.css"
+  },
+  {
+    "revision": "4baf5c08a63efaafe193",
+    "url": "/static/js/2.ac2ea71d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ac2ea71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e665e3d0f65ec67d9273",
+    "url": "/static/js/main.af7b347a.chunk.js"
+  },
+  {
+    "revision": "d979a88d82367b2ad12f",
+    "url": "/static/js/runtime-main.a1ade315.js"
+  }
+]);
